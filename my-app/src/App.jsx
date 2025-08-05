f4c00fc923fd92c7a6886add10882d2c86a38665
Saima Hussain <hussainsaima2023@gmail.com>
2025-08-05 14:45:53 +0500

    Commit after changes

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+function Header() {
+  return <h1>User Profiles</h1>;
+}
+
+function UserCard({ name, role }) {
+  return (
+    <div className="user-card">
+      <h2>Name: {name}</h2>
+      <p>Role: {role}</p>
+    </div>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const userName = "Saima Hussain ";
+  const userRole = "Front-end Web Developer";
+  const user2Name = "Azaz ur Rehman";
+  const user2Role = "Front-end Web Developer"
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+
+      <UserCard name={userName} role={userRole} />
+      <UserCard name={user2Name} role={user2Role} />
+
+      <UserCard name="Maqool Ahmed" role="Graphic Designer Designer" />
+      <UserCard name="Amna" role="WordPress Developer" />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
